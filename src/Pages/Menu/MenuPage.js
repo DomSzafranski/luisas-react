bddf0e1e3cf72f974ca66307518aa47f4553ae1f
DomSzafranski <domszafranski@gmail.com>
2023-03-09 18:55:24 +0000

    Meta tags on menu page

--- src/Pages/Menu/MenuPage.js
+++ src/Pages/Menu/MenuPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { MenuBanner } from "./MenuBanner";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
@@ -6,27 +7,33 @@ import { Outlet } from "react-router-dom";
 import './menu.css';
 
 
-export const MenuPage = () => { 
+export const MenuPage = () => {
 
 
     const heroImgSrc = require("../../Media/luisas-menu-wide.jpg");
 
     return (
         <div>
+            <Helmet>
+                <title>Menu | Luisa's Italian Restaurant</title>
+                <meta charset="UTF-8" />
+                <meta name="description" content="Menu for Luisa's Italian restaurant in Radcliffe" />
+            </Helmet>
+
             <MenuBanner
                 src={heroImgSrc}
                 alt="Luisa's Italian Restaurant Menu"
             />
             <div className="MenuPageContainer">
-            <nav className="navLinksContainer">
-                <Link to='./'><button className="cta-button">Main Menu</button></Link>
-                <Link to='./dessert'><button className="cta-button">Dessert Menu</button></Link>
-                <Link to='./bar'><button className="cta-button">Bar Menu</button></Link>
-                <Link to='./mothers-day'><button className="cta-button">Mother's Day 2023</button></Link>
-            </nav>
-            {/* displays the menus based on the url & sub path controlled by buttons above 
+                <nav className="navLinksContainer">
+                    <Link to='./'><button className="cta-button">Main Menu</button></Link>
+                    <Link to='./dessert'><button className="cta-button">Dessert Menu</button></Link>
+                    <Link to='./bar'><button className="cta-button">Bar Menu</button></Link>
+                    <Link to='./mothers-day'><button className="cta-button">Mother's Day 2023</button></Link>
+                </nav>
+                {/* displays the menus based on the url & sub path controlled by buttons above 
             passes the parent state down to each child element*/}
-            
+
                 <Outlet />
 
             </div>
