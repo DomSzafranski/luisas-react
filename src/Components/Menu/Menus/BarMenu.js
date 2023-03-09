146bdc61015f41932466b86d2445b6b9815efa79
DomSzafranski <domszafranski@gmail.com>
2023-03-09 13:47:17 +0000

    Dietary tags on menu

---
+++ src/Components/Menu/Menus/BarMenu.js
@@ -0,0 +1,14 @@
+export const menuItems = [
+    {
+        name: 'margherita',
+        price: '9.65',
+        description: 'Tomato sauce, fior di latte, fresh basil, extra virgin olive oil',
+        category: 'Wine',
+        highlight: false,
+        allergens: [],
+        dietary: []
+    },
+    
+
+    
+];
--- src/Components/WhatsOn/Events.js
+++ src/Components/WhatsOn/Events.js
@@ -5,14 +5,14 @@ export const events = [
         date: 'Sunday March 19th',
         buttonText: "View Menu",
         buttonPath: '/menu/mothers-day',
-        img: require('../../Media/mothers-day-2023-banner'),
+        img: require('../../Media/mothers-day-2023-banner.png'),
         imgAlt: 'Book a table for Mother\'s day at Luisa\'s',
         tagName: "",
         tagEndDate: ""
     },
     {
         title: "Spring Menu",
-        description: "Let's welcome in the start of brighter, warmer days with our new Spring Menu.We're highlighting some seasonal favourites like XXX and XXX.",
+        description: "Let's welcome in the start of brighter, warmer days with our new Spring Menu.",
         date: 'March - May',
         buttonText: "View Menu",
         buttonPath: '/menu',
@@ -31,27 +31,5 @@ export const events = [
         imgAlt: '',
         tagName: "",
         tagEndDate: ""
-    },
-    {
-        title: "30-Hour Sourdough Pizza",
-        description: "With a long, slow, 30-hour prove and baked in as little as 90 seconds in our wood fired oven, our unique Sourdough Crust is wonderfully light and chewey. Our dough is made fresh daily at our open-plan Italian kitchen in Radcliffe.",
-        date: '',
-        buttonText: "Book a Table",
-        buttonPath: '/',
-        img: '/',
-        imgAlt: '',
-        tagName: "",
-        tagEndDate: "2023/01/20"
-    },
-    {
-        title: "Two for One Cocktail Night",
-        description: "It’s never too early in the week for cocktails, is it? Invite a couple friends and join us every Wednesday night and enjoy half off your drinks bill – no fuss!",
-        date: '',
-        buttonText: "Book a Table",
-        buttonPath: '/',
-        img: require('../../Media/241-cocktails.jpg'),
-        imgAlt: '',
-        tagName: "",
-        tagEndDate: "2023/01/20"
-    },
+    }
 ]
